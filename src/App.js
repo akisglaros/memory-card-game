e3c5a51a1ba8b2661269dad9e5257f71dde496d5
Akis Glaros <akis222@gmail.com>
2019-07-28 21:01:30 +0300

    Change App component to include App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Board from "./components/Board";
+import {getPhotos} from "./helpers/photoHandler";
 
 const App = () => {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <Board images={getPhotos()} />
+        </div>
+    );
 };
 
 export default App;
